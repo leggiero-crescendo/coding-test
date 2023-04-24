0f793d75444e90ca5ede151f2a106793bedeb0c5
leggiero-crescendo <125112464+leggiero-crescendo@users.noreply.github.com>
2023-04-24 23:50:50 +0900

    Create 요격시스템.cpp

---
+++ CPP/요격시스템.cpp
@@ -0,0 +1,20 @@
+/* 
+💡 Targets
+- A로부터 받은 공격 미사일을 격추하는 최소 요격 미사일 수를 return하라
+- y 축에 수평인 미사일로 공격하여 한번에 요격가능하며, 개구간 내에서만 가능함
+
+🔑 Strategy
+- 
+🔑 Method
+- 
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> targets) {
+    int answer = 0;
+    return answer;
+}
