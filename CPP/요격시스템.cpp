821388448cdc63d5c1b35654f54b084e0348ec15
leggiero-crescendo <125112464+leggiero-crescendo@users.noreply.github.com>
2023-04-27 00:22:40 +0900

    Update 요격시스템.cpp

--- CPP/요격시스템.cpp
+++ CPP/요격시스템.cpp
@@ -47,3 +47,34 @@ Solution 1
 테스트 10 〉	통과 (91.89ms, 77.8MB)
 테스트 11 〉	통과 (0.01ms, 4.14MB)
 */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool cmp(const vector<int>& a, const vector<int>& b) {
+  cout<< "a" << a[1] <<"b"<< b[1];  
+  return a[1] < b[1];
+}
+
+int solution2(vector<vector<int>> targets) {
+  sort(targets.begin(), targets.end(), cmp);
+
+  int ans = 0;
+  int t = -1;
+  for (auto& tar: targets) {
+    if (tar[0] >= t) {
+      t = tar[1];
+      ans++;
+    }
+  }
+
+  return ans;
+}
+
+/* 
+비교함수를 사용하던지, begin과 end를 모두 검사해주는 항목이 있던지 해야했다.
+내가 만든 정답은 [[1 5] [2 3] [3 4]] 라는 순서로 소팅 될 수 있으며, 이에 따라 정답은 2이지만 1이라는 결과를 내보낼 수 있다. 
+sort의 compare를 통해서 end 끼리 비교하도록 한다면 
+23 / 34 / 15 의 순으로 결과를 보낼 것이다.
+*/
